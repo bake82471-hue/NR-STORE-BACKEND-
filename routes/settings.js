dd749c4f4de81397f45d23b3369dc868f17dd83a
bake82471-hue <bake82471@gmail.com>
2026-01-14 00:29:54 +0100

    Update settings.js

--- routes/settings.js
+++ routes/settings.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/adminController");
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../middleware/auth"); // FIXED
 
 // GET /settings
 router.get("/", adminController.getSettings);
