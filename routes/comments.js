3c4d56de6fee289b49958b31e758036597a8c99c
bake82471-hue <bake82471@gmail.com>
2026-01-15 17:57:34 +0100

    Add files via upload

--- routes/comments.js
+++ routes/comments.js
@@ -1,12 +1,54 @@
-// backend/routes/comments.js
 const express = require("express");
 const router = express.Router();
-const commentsController = require("../controllers/commentsController");
+const fs = require("fs");
+const path = require("path");
 
-// GET /items/:id/comments
-router.get("/:id/comments", commentsController.getForItem);
+const COMMENTS_FILE = path.join(__dirname, "../data/comments.json");
 
-// POST /items/:id/comments
-router.post("/:id/comments", commentsController.addForItem);
+// Ensure file exists
+if (!fs.existsSync(COMMENTS_FILE)) {
+    fs.writeFileSync(COMMENTS_FILE, "{}");
+}
+
+// Load comments
+function loadComments() {
+    return JSON.parse(fs.readFileSync(COMMENTS_FILE, "utf8"));
+}
+
+// Save comments
+function saveComments(data) {
+    fs.writeFileSync(COMMENTS_FILE, JSON.stringify(data, null, 2));
+}
+
+// GET comments for item
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    const comments = loadComments();
+    res.json(comments[id] || []);
+});
+
+// POST new comment
+router.post("/:id", (req, res) => {
+    const id = req.params.id;
+    const { username, comment } = req.body;
+
+    if (!username || !comment) {
+        return res.status(400).json({ error: "Missing fields" });
+    }
+
+    const comments = loadComments();
+
+    if (!comments[id]) comments[id] = [];
+
+    comments[id].push({
+        username,
+        comment,
+        date: new Date().toLocaleString("it-IT")
+    });
+
+    saveComments(comments);
+
+    res.json(comments[id]);
+});
 
 module.exports = router;
